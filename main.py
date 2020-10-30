5b936f1fa97baf9e862483add99089f4cc39ad61
sikender shahid <sikendershahid91@gmail.com>
2020-10-30 15:46:57 -0700

    display structure

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
 import src.image_processing as imp
 
+def defaultImage():
+    image = mpimg.imread( 'images/solidWhiteRight.jpg' )
+    plt.imshow(image)
+    plt.show()
+
 if __name__ == '__main__':
-    print( imp.get_canny_filter() ) 
+    print( imp.get_canny_filter() )
+
+    defaultImage()
+    
+    
